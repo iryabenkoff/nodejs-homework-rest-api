da88b4abb0330001ced9bcf43768e6685ecc008c
Igor Ryabenko <100082550+iryabenkoff@users.noreply.github.com>
2022-10-04 13:33:35 +0300

    hw6 done

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const RequestError = require("./RequestError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseSchemaError = require("./handleMongooseSchemaError");
+const verifyMessage = require("./verifyMessage");
 
 module.exports = {
   RequestError,
   ctrlWrapper,
   handleMongooseSchemaError,
+  verifyMessage,
 };
---
+++ helpers/verifyMessage.js
@@ -0,0 +1,16 @@
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const verifyMessage = async (msg) => {
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body);
+    }
+  }
+};
+
+module.exports = verifyMessage;
